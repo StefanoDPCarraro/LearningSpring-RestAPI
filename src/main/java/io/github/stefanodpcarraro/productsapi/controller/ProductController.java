a7b3805f117604bc8ae08cbe4ce9f067181f4403
Stefano Carraro <stefano.c@edu.pucrs.br>
2024-12-05 16:36:17 -0300

    Added deleteById

--- src/main/java/io/github/stefanodpcarraro/productsapi/controller/ProductController.java
+++ src/main/java/io/github/stefanodpcarraro/productsapi/controller/ProductController.java
@@ -6,6 +6,7 @@ import io.github.stefanodpcarraro.productsapi.repository.ProductRepository;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,5 +35,10 @@ public class ProductController {
     @GetMapping("/{id}")
     public Product getById(@PathVariable String id){
         return productRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable String id){
+        productRepository.deleteById(id);
     }
 }
