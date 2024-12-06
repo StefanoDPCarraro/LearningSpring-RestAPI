50cb3bc62bf025c20640c7634ba81257dbfab8a6
Stefano Carraro <stefano.c@edu.pucrs.br>
2024-12-06 19:31:03 -0300

    Added update method

--- src/main/java/io/github/stefanodpcarraro/productsapi/controller/ProductController.java
+++ src/main/java/io/github/stefanodpcarraro/productsapi/controller/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,5 +41,11 @@ public class ProductController {
     @DeleteMapping("/{id}")
     public void deleteById(@PathVariable String id){
         productRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void update(@PathVariable String id, @RequestBody Product product){
+        product.setId(id);
+        productRepository.save(product);
     }
 }
